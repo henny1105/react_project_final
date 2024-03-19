0676222445adca395a2b8bc74019bbc4ce1f8400
henny <kimhyein1105@gmail.com>
2024-03-19 18:24:14 +0900

    박스 만들기

---
+++ src/Components/Box.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Box = (props) => {
+	return (
+		<div className='box'>
+			<h1>{props.title}</h1>
+			<img
+				src='https://file.moongori.com/firstmall/data/goods/3274/1096790/2022/06/tmp_751d6e6bfa36efbacd033c7107db5ff55539view.jpg'
+				jsaction='VQAsE'
+				class='item-img'
+				alt='문고리닷컴 : 셀프인테리어 한번에 검색, 손쉽게 쇼핑'
+				jsname='kn3ccd'
+				aria-hidden='false'
+			></img>
+			<h2>WIN</h2>
+		</div>
+	);
+};
+
+export default Box;
