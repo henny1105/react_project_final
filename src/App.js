7fea3b36efd5c9cb6d1668e6194cf9b69e7aa497
henny <kimhyein1105@gmail.com>
2024-03-19 18:48:49 +0900

    버튼 클릭시 해당 관련된 이미지 나오게 하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Box from './Components/Box';
 
@@ -8,17 +8,37 @@ import Box from './Components/Box';
 // 4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
 // 5. 3번 4번의 결과를 가지고 누가 이겼는지 승패를 따진다.
 // 6. 승패결과에 따라 테두리 색이 바뀐다. (이기면 초록, 지면 빨강, 비기면 검정색)
+
+const choice = {
+	rock: {
+		name: 'Rock',
+		img: 'https://i.namu.wiki/i/EbHl4I2dCr3aoC7AFjMYv7zBAFQTE0Cr0-r2XiIKLakxARH3BY9eonE3AZ2_ctET_2vpLI-piN4F224wAUdyyQ.webp',
+	},
+	scissors: {
+		name: 'Scissors',
+		img: 'https://img.animalplanet.co.kr/news/2020/05/20/700/al43zzl8j3o72bkbux29.jpg',
+	},
+	paper: {
+		name: 'Paper',
+		img: 'https://pbs.twimg.com/media/CXuloe2UQAAdoMJ.jpg',
+	},
+};
+
 function App() {
+	const [userSelect, setUserSelect] = useState(null);
+	const play = (userChoice) => {
+		setUserSelect(choice[userChoice]);
+	};
 	return (
 		<div>
 			<div className='main'>
-				<Box title='You' />
-				<Box title='Computer' />
+				<Box title='You' item={userSelect} />
+				{/* <Box title='Computer' /> */}
 			</div>
 			<div className='main'>
-				<button>가위</button>
-				<button>바위</button>
-				<button>보</button>
+				<button onClick={() => play('scissors')}>가위</button>
+				<button onClick={() => play('rock')}>바위</button>
+				<button onClick={() => play('paper')}>보</button>
 			</div>
 		</div>
 	);
--- src/Components/Box.js
+++ src/Components/Box.js
@@ -4,14 +4,7 @@ const Box = (props) => {
 	return (
 		<div className='box'>
 			<h1>{props.title}</h1>
-			<img
-				src='https://file.moongori.com/firstmall/data/goods/3274/1096790/2022/06/tmp_751d6e6bfa36efbacd033c7107db5ff55539view.jpg'
-				jsaction='VQAsE'
-				class='item-img'
-				alt='문고리닷컴 : 셀프인테리어 한번에 검색, 손쉽게 쇼핑'
-				jsname='kn3ccd'
-				aria-hidden='false'
-			></img>
+			<img className='item-img' src={props.item && props.item.img} />
 			<h2>WIN</h2>
 		</div>
 	);
