79546efd75f8828ba5fc3ecfe9793fd8c12f7eb6
henny <kimhyein1105@gmail.com>
2024-03-24 23:34:29 +0900

    로딩 스피너 추가, 도시 이름 클릭했을 때 도시 정보 날씨 정보 불러오기

--- src/Components/sub/weather/Weather.jsx
+++ src/Components/sub/weather/Weather.jsx
@@ -3,16 +3,13 @@ import WeatherBox from './WeatherBox';
 import WeatherButton from './WeatherButton';
 import './Weather.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// 1. 앱이 실행되자마자 실행기반의 날씨 정보가 보인다.
-// 2. 날씨 정보에는 도시, 섭씨, 화씨 날씨 상태 정보가 들어간다.
-// 3. 5개의 버튼이 있다.(1개는 현재 위치, 4개는 다른 도시)
-// 4. 도시버튼을 클릭할 때 마다 도시별 날씨가 나온다.
-// 5. 현재 위치 버튼을 누르면 다시 현재 위치 기반의 날씨가 나온다.
-// 6. 데이터를 들고 오는 동안 로딩 스피너가 돈다.
+import ClipLoader from 'react-spinners/ClipLoader';
 
 function Weather() {
 	const [weather, setWeather] = useState(null);
+	const [city, setCity] = useState('');
+	const [loading, setLoading] = useState(false);
+	const cities = ['tokyo', 'seoul', 'beijing', 'sydney', 'london'];
 
 	const getWeatherByCurrentLocation = async (lat, lon) => {
 		let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=84bbf1bc16c21571bb35b7478e7b2d24&units=metric`;
@@ -20,6 +17,7 @@ function Weather() {
 			let response = await fetch(url);
 			let data = await response.json();
 			setWeather(data);
+			setLoading(false);
 		} catch (error) {
 			console.error(error);
 		}
@@ -28,20 +26,44 @@ function Weather() {
 	useEffect(() => {
 		const getCurrentLocation = () => {
 			navigator.geolocation.getCurrentPosition((position) => {
-				let lat = position.coords.latitude;
-				let lon = position.coords.longitude;
-				getWeatherByCurrentLocation(lat, lon);
+				getWeatherByCurrentLocation(position.coords.latitude, position.coords.longitude);
 			});
 		};
 
 		getCurrentLocation();
 	}, []);
 
+	const getWeatherByCity = async (selectedCity) => {
+		setLoading(true);
+		let url = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=84bbf1bc16c21571bb35b7478e7b2d24&units=metric`;
+		try {
+			let response = await fetch(url);
+			let data = await response.json();
+			setWeather(data);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		if (city !== '') {
+			getWeatherByCity(city);
+		}
+	}, [city]);
+
 	return (
 		<div className='weather_project'>
 			<div className='container'>
-				<WeatherBox weather={weather} />
-				<WeatherButton />
+				{loading ? (
+					<ClipLoader color='#f88c6b' loading={loading} size={150} aria-label='Loading Spinner' data-testid='loader' />
+				) : (
+					<>
+						<WeatherBox weather={weather} />
+						<WeatherButton cities={cities} setCity={setCity} />
+					</>
+				)}
 			</div>
 		</div>
 	);
--- src/Components/sub/weather/WeatherButton.jsx
+++ src/Components/sub/weather/WeatherButton.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const WeatherButton = () => {
+const WeatherButton = ({ cities, setCity }) => {
+	// setCity props 추가
 	return (
-		<div>
-			<div className='button_box'>
-				<Button variant='info'>Current Location</Button> <Button variant='info'>Tokyo</Button> <Button variant='info'>Beijing</Button>
-				<Button variant='info'>Newyork</Button>
-			</div>
+		<div className='button_box'>
+			{cities.map((item, index) => (
+				<Button key={index} variant='info' onClick={() => setCity(item)}>
+					{' '}
+					{item}
+				</Button>
+			))}
 		</div>
 	);
 };
