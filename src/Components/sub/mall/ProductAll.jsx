f51fa1f8e1149112039e0f530c7db41ba1376f3c
henny <kimhyein1105@gmail.com>
2024-03-30 01:12:44 +0900

    반응형 추가

--- src/Components/sub/mall/ProductAll.jsx
+++ src/Components/sub/mall/ProductAll.jsx
@@ -3,9 +3,26 @@ import { useSearchParams } from 'react-router-dom';
 import ProductCard from '../../ProductCard';
 import { Container, Row, Col } from 'react-bootstrap';
 
+const useWindowSize = () => {
+	const [size, setSize] = useState([window.innerWidth]);
+	useEffect(() => {
+		const handleResize = () => {
+			setSize([window.innerWidth]);
+		};
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	return size;
+};
+
 const ProductAll = () => {
+	const width = useWindowSize();
+
 	const [productList, setProductList] = useState([]);
 	const [query] = useSearchParams();
+
 	const getProducts = async () => {
 		let searchQuery = query.get('q') || '';
 		let url = `https://my-json-server.typicode.com/henny1105/react_project_final/products/?q=${searchQuery}`;
@@ -23,7 +40,7 @@ const ProductAll = () => {
 			<Container className='mall_project'>
 				<Row>
 					{productList.map((product) => (
-						<Col lg={2} key={product.id}>
+						<Col lg={width >= 1440 ? 2 : width <= 989 ? 3 : 3} key={product.id}>
 							<ProductCard item={product} />
 						</Col>
 					))}
