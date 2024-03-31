af7ac5d4fb3d50712745bed91ed2d6776bb91af9
henny <kimhyein1105@gmail.com>
2024-03-31 23:45:39 +0900

    감소 버튼 만들기, css 주기

--- src/Components/sub/redux_practice/Redux.jsx
+++ src/Components/sub/redux_practice/Redux.jsx
@@ -1,19 +1,43 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BoxRedux from './BoxRedux';
+import './Redux.css';
 
 const Redux = () => {
-	const count = useSelector((state) => state.count);
-	const dispatch = useDispatch();
+	let count = useSelector((state) => state.count);
+	let id = useSelector((state) => state.id);
+	let password = useSelector((state) => state.password);
+	let dispatch = useDispatch();
 
 	const increase = () => {
-		dispatch({ type: 'INCREMENT' });
+		dispatch({ type: 'INCREMENT', payload: { num: 1 } });
+	};
+
+	const decrease = () => {
+		dispatch({ type: 'DECREMENT', payload: { num: 1 } });
+	};
+
+	const login = () => {
+		dispatch({ type: 'LOGIN', payload: { id: 'hyein', password: '123' } });
 	};
 
 	return (
-		<div>
+		<div className='redux_box'>
+			<h1>
+				{id},{password}
+			</h1>
 			<h1>{count}</h1>
-			<button onClick={increase}>증가</button>
+			<div className='button_box'>
+				<button className='increase' onClick={increase}>
+					증가
+				</button>
+				<button className='decrease' onClick={decrease}>
+					감소
+				</button>
+				<button className='login_button' onClick={login}>
+					Login
+				</button>
+			</div>
 			<BoxRedux />
 		</div>
 	);
