5389abb6f5de309a5b945a44ebe1cb4bec09107d
henny <kimhyein1105@gmail.com>
2024-04-03 18:04:40 +0900

    productReducer 합치기 작업

--- src/Components/sub/mall/redux/reducers/productReducer.js
+++ src/Components/sub/mall/redux/reducers/productReducer.js
@@ -1,14 +1,23 @@
 let initialState = {
 	productList: [],
+	selectedItem: null,
+	loading: false,
 };
 
 function productReducer(state = initialState, action) {
 	let { type, payload } = action;
+
 	switch (type) {
 		case 'GET_PRODUCT_SUCCESS':
-			return { ...state, productList: payload.data };
+			return { ...state, productList: payload.data, loading: false };
+		case 'GET_SINGLE_PRODUCT_SUCCESS':
+			return { ...state, selectedItem: payload.data, loading: false };
+		case 'GET_PRODUCT_DETAIL_START':
+			return { ...state, loading: true };
+		case 'GET_PRODUCT_DETAIL_FAILURE':
+			return { ...state, loading: false };
 		default:
-			return {...state};
+			return { ...state };
 	}
 }
 
